ccf60c72c98fdbe2f6535928f162c1790a12ef4c
oneStrike <759994689@qq.com>
2020-11-10 18:50:24 +0800

    41

--- components-lk/remarks/remarks.vue
+++ components-lk/remarks/remarks.vue
@@ -1,11 +1,11 @@
 <template>
-  <view class="remarks_container">
-    <popUpLayer>
-      <template>
-        <view class="remarks_content">
+  <view class="remarks_container" @touchmove.stop.prevent = "touchmoveStop">
+        <view class="remarks_content"
+              :style="{'bottom':keyboardHeight+'px'}"
+              @touchmove.stop.prevent = "touchmoveStop">
           <view class="remarks_head">
             <text>訂單備註</text>
-            <uni-icons @click="$emit('close-remarks',0)"
+            <uni-icons @click="$emit('close-remarks',remarksSelect)"
                        class="clear_remarks"
                        type="clear"
                        size="44"
@@ -13,16 +13,56 @@
             </uni-icons>
           </view>
           <view class="remarks_options">
-            <view class="option">
+            <view class="custom_remarks">
+              <!-- #ifdef APP-PLUS-->
+              <textarea type="text"
+                        focus
+                        maxlength="80"
+                        v-model="remarksInput">
+              </textarea>
+              <!-- #endif-->
+              <!-- #ifdef MP-WEIXIN-->
+              <textarea type="text"
+                        maxlength="80"
+                        fixed
+                        :show-confirm-bar="false"
+                        :adjust-position="false"
+                        v-model="remarksInput">
+              </textarea>
+              <!-- #endif-->
+            </view>
+            <view class="preset">
+              <text @touchend.prevent="selectRemarks(index,true)"
+                  v-for="(content,index) in remarks"
+                  :key="index">{{content}}</text>
+            </view>
+            <!--<view class="option" v-for="(content,index) in remarks" :key="index">
               <view class="options_title">
-                <text>無接觸配送：</text>
+                <text>{{content}}：</text>
               </view>
               <view class="options_btn">
-                <button @click="selectRemarks('noContact',null)" :class="{'selected':!noContact}" plain>不需要</button>
-                <button @click="selectRemarks('noContact','無接觸配送')" :class="{'selected':noContact}" plain>需要</button>
+                <button @click="selectRemarks(index,false)"
+                        :class="{'selected':selectArr.indexOf(content) === -1}"
+                        plain>不需要</button>
+                <button @click="selectRemarks(index,true)"
+                        :class="{'selected':selectArr.indexOf(content) !== -1}"
+                        plain>需要</button>
               </view>
-            </view>
-            <view class="option">
+            </view>-->
+            <!--<view class="option" v-for="(content,index) in remarks" :key="index">
+              <view class="options_title">
+                <text>{{content}}：</text>
+              </view>
+              <view class="options_btn">
+                <button @click="selectRemarks(index,false)"
+                        :class="{'selected':selectArr.indexOf(content) === -1}"
+                        plain>不需要</button>
+                <button @click="selectRemarks(index,true)"
+                        :class="{'selected':selectArr.indexOf(content) !== -1}"
+                        plain>需要</button>
+              </view>
+            </view>-->
+<!--            <view class="option">
               <view class="options_title">
                 <text>纸巾：</text>
               </view>
@@ -39,14 +79,13 @@
                 <button @click="selectRemarks('sugar',null)" :class="{'selected':!sugar}" plain>不需要</button>
                 <button @click="selectRemarks('sugar','糖包')" :class="{'selected':sugar}" plain>需要</button>
               </view>
+            </view>-->
+            <view @click="$emit('close-remarks',remarksInput||selectArr.join())"
+                  class="remarks_confirm">
+              <button plain>確定</button>
             </view>
           </view>
         </view>
-        <view @click="$emit('close-remarks',{noContact,paper,sugar})" class="remarks_confirm">
-          <button plain>確定</button>
-        </view>
-      </template>
-    </popUpLayer>
   </view>
 </template>
 
@@ -56,26 +95,58 @@ export default {
   name: "remarks",
   data() {
     return {
-      noContact: true,
-      paper: true,
-      sugar: true,
+     selectIndex:0,
+      selectArr:[],
+      remarksInput:'',
+      keyboardHeight:0,
     }
   },
   props:{
     remarks:{
       type:Array,
       default:()=>[]
+    },
+    remarksSelect:{
+      type:String,
+      default:''
     }
   },
-  mounted() {
-    this.noContact = this.remarks.noContact;
-    this.paper = this.remarks.paper;
-    this.sugar = this.remarks.sugar;
+  mounted(){
+    //基本类型的深度拷贝
+    // this.selectArr = Object.assign([],this.remarksSelect)
+    this.remarksInput = this.remarksSelect
+    // #ifdef MP-WEIXIN
+    uni.onKeyboardHeightChange((res) => {
+     this.$nextTick(()=>{
+       console.log(res.height);
+       this.keyboardHeight = res.height
+     })
+    })
+    // #endif
   },
   methods: {
-    selectRemarks(t, v) {
-      this[t] = v;
+    selectRemarks(i, s) {
+      this.remarksInput += this.remarks[i] +','
+      // console.log(i);
+      // let remarksContent= this.selectArr.indexOf(this.remarks[i])
+      // if( remarksContent === -1){
+      //   this.selectArr.push(this.remarks[i])
+      // }else{
+      //   let remarksIndex = this.selectArr.indexOf(this.remarks[i])
+      //   this.selectArr.splice(remarksIndex, 1)
+      // }
+      // this.selectIndex = i;
+      // console.log(i);
+      // if (s) {
+      //   this.selectArr.push(this.remarks[i])
+      // }
+      // else {
+      //   let remarksIndex = this.selectArr.indexOf(this.remarks[i])
+      //   this.selectArr.splice(remarksIndex, 1)
+      // }
+      // console.log(this.selectArr);
     },
+    touchmoveStop(){},
   },
   components:{
     popUpLayer
@@ -106,6 +177,12 @@ button {
 }
   .remarks_container{
     width: 100%;
+    height: 100vh;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background: rgba(0, 0, 0, .5);
+    z-index: 1231321;
 
     .remarks_content{
       width: 100vw;
@@ -113,6 +190,9 @@ button {
       background:#FFFFFF;
       border-radius: 20rpx 20rpx 0rpx 0rpx;
       overflow: hidden;
+      position: absolute;
+      bottom: 0;
+      left:0;
 
       .remarks_head{
         width: 100%;
@@ -141,6 +221,36 @@ button {
       padding: 0 $remarks-spacing;
       box-sizing: border-box;
 
+      .custom_remarks{
+        height: 230rpx;
+        padding: 10rpx;
+        box-sizing: border-box;
+        margin: 20rpx 0;
+        border: 2rpx solid #ccc;
+        overflow: hidden;
+      }
+
+      .preset{
+        width: 100%;
+        height: 60rpx;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        font-size: $font-size-base;
+
+        text{
+          display: block;
+          padding:10rpx 20rpx;
+          border: 1rpx solid #ccc;
+        }
+
+        .selected{
+          background: rgba(23,161,255,0.1);
+          border: 1rpx solid $main-color;
+          color: $main-color;
+        }
+      }
+
       .option{
         width: 100%;
         display: flex;
@@ -156,8 +266,6 @@ button {
         }
         .options_btn{
           display: flex;
-
-
         }
       }
     }
@@ -165,9 +273,7 @@ button {
       width: 650rpx;
       height: 82rpx;
       border-radius: 10rpx;
-      position: fixed;
-      bottom: 38rpx;
-      left: $remarks-spacing;
+      margin-top: 60rpx;
 
       button{
         width: 100%;
